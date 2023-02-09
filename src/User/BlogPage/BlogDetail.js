c0ff4c1ca91f974f88d0b7a1ff81bf175b10808b
Aadebukola <oluwadamilola.adebukola@yahoo.com>
2023-02-09 13:07:08 -0800

    Blog

--- src/User/BlogPage/BlogDetail.js
+++ src/User/BlogPage/BlogDetail.js
@@ -14,9 +14,9 @@ const BlogDetail = () => {
   return (
     <div>
         <div className='blog-title mt-5'>
-            <h5 className='text-start ms-sm-5 pt-3 f-style fs-4'>Blog {'>'} A Guide To New Orleans' Best...</h5>
+            <h5 className='text-start ms-sm-2 pt-3 f-style fs-4'>Blog {'>'} A Guide To New Orleans' Best...</h5>
         </div>
-        <div className='blog d-sm-flex text-center ms-5 mt-4'>
+        <div className='blog d-sm-flex text-center ms-2 mt-4'>
           <aside className='blogAside f-style fs-5'>
              <BlogSide/>
           </aside>
