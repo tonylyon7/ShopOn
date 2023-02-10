1744c36b349137cd96344839f2b2c22da804065b
okparajohnbosco <okparajohnbosco570@gmail.com>
2023-02-10 11:40:12 +0100

    ShopOn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './User/Landingpage/Landing';
 import Blog from './User/BlogPage/Blog';
+import About from './User/AboutPage/About';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={ <Landing/> } />
+          <Route path='/About' element={<About/>}/>
           <Route path='/Blog' element={ <Blog/> } />
         </Routes>
       </Router>
