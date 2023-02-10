9b355fe8826c4035dcc0da1a90588d8ed9cbe103
okparajohnbosco <okparajohnbosco570@gmail.com>
2023-02-10 12:23:37 +0100

    Blog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './User/Landingpage/Landing';
 import Blog from './User/BlogPage/Blog';
+import About from './User/AboutPage/About';
+import BlogDetail from './User/BlogPage/BlogDetail'
 
 
 
