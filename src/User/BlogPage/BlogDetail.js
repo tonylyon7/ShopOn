f4f6eff66f6f7e5d299ffc15ac283eb176bc706d
Jephthah <jephthahndukwe@gmail.com>
2023-02-12 17:59:23 +0100

    Blog

--- src/User/BlogPage/BlogDetail.js
+++ src/User/BlogPage/BlogDetail.js
@@ -18,7 +18,7 @@ const BlogDetail = () => {
     <div>
         <Navbar/>
         <div className='blog-title'>
-            <h5 className='text-start ms-sm-5 pt-3 f-style fs-4'>Blog {'>'} A Guide To New Orleans' Best...</h5>
+            <h5 className='text-start ms-5 pt-3 f-style fs-4'>Blog {'>'} A Guide To New Orleans' Best...</h5>
         </div>
         <div className='blog d-sm-flex text-center ms-2 mt-4'>
           <aside className='blogAside f-style fs-5'>
