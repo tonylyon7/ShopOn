493628a3a8361e24ee4f7ed23b1f55b8d9307857
Chisom <chisomemmanuel995@gmail.com>
2023-02-18 09:30:15 +0100

    new change

--- src/User/Landingpage/Landing.js
+++ src/User/Landingpage/Landing.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import SignIn from '../SignUp/SignIn.js'
-import Main from './Main.js'
-import Nav from './Nav.js'
-import Register from '../SignUp/Register.js'
-import Footer from './Footer.js'
-import SignIn from '../SignUp/SignIn.js'
-import './CSS/main.css'
+import React from "react"
+import SignIn from "../SignUp/SignIn.js"
+import Main from "./Main.js"
+import Nav from "./Nav.js"
+import Register from "../SignUp/Register.js"
+import Footer from "./Footer.js"
 // import SignIn from '../SignUp/SignIn.js'
-
+import "./CSS/main.css"
+// import SignIn from '../SignUp/SignIn.js'
 
 const Landing = () => {
   return (
     <>
-    <Nav/>
-    {/* <SignIn/> */}
-    <Register/>
-    <Main/>
-    <Footer/>
+      <Nav />
+      {/* <SignIn/> */}
+      <Register />
+      <Main />
+      <Footer />
     </>
   )
 }
